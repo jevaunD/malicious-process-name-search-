e0f21a9b578bb2b657061b2ef18724e0a399f08f
jevii_jay <99161820+jevaunD@users.noreply.github.com>
2024-02-05 21:21:25 -0500

    Update clean_txt.py

--- clean_txt.py
+++ clean_txt.py
@@ -1,4 +1,4 @@
 #This removes the square brackets and apostrophes from the malicious processes' names
-with open('clean.csv', 'r') as clean:
+with open('process_names.txt', 'r') as clean:
     for line in clean:
         print(line[2:-3])
